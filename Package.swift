61d88a2b447ce9e1902d8d86e6cc2f3cb1ab0ed6
Antoine Marandon <antoine@marandon.fr>
2025-09-29 10:44:43 +0200

    Update to support iOS 26

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/FluidGroup/swift-indexed-collection", from: "0.2.1"),
-    .package(url: "https://github.com/siteline/swiftui-introspect", from: "1.3.0"),
+    .package(url: "https://github.com/siteline/swiftui-introspect", from: "26.0.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -44,7 +44,7 @@ let package = Package(
       name: "CollectionView",
       dependencies: [
         "ScrollTracking",
-        .product(name: "IndexedCollection", package: "swift-indexed-collection"),        
+        .product(name: "IndexedCollection", package: "swift-indexed-collection"),
       ]
     ),
     .target(
